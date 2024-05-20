cd8160b5b1281e25001daa5fbf68970d878e02b5
Galileo Luna <gluna@getontop.com>
2024-05-20 19:26:31 -0300

    Remove Dockerfile and update Dockerfile content in create.go
    Deleted standalone Dockerfile from repository and moved its content directly into the `create.go` file. Now, the Dockerfile content is generated dynamically when the create command is executed.

--- cmd/create.go
+++ cmd/create.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/spf13/cobra"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -185,14 +184,51 @@ volumes:
 	}
 
 	// Copy Dockerfile
-	dockerfileSrc := "cmd/Dockerfile"
+	dockerfileContent := fmt.Sprintf(`# syntax=docker/dockerfile:1.4
+FROM --platform=$BUILDPLATFORM golang:lastest AS builder
+
+WORKDIR /code
+
+ENV CGO_ENABLED 0
+ENV GOPATH /go
+ENV GOCACHE /go-build
+
+COPY go.mod go.sum ./
+RUN --mount=type=cache,target=/go/pkg/mod/cache \
+    go mod download
+
+COPY static .
+
+RUN --mount=type=cache,target=/go/pkg/mod/cache \
+    --mount=type=cache,target=/go-build \
+    go build -o bin/backend main.go
+
+CMD ["/code/bin/backend"]
+
+FROM builder as dev-envs
+
+RUN <<EOF
+apk update
+apk add git
+EOF
+
+RUN <<EOF
+addgroup -S docker
+adduser -S --shell /bin/bash --ingroup docker vscode
+EOF
+
+# install Docker tools (cli, buildx, compose)
+COPY --from=gloursdocker/docker / /
+
+CMD ["go", "run", "main.go"]
+
+FROM scratch
+COPY --from=builder /code/bin/backend /usr/local/bin/backend
+CMD ["/usr/local/bin/backend"]
+`)
 	dockerfileDst := filepath.Join(projectName, "Dockerfile")
-	dockerfileContent, err := ioutil.ReadFile(dockerfileSrc)
-	if err != nil {
-		fmt.Printf("Error reading Dockerfile: %v\n", err)
-		return
-	}
-	if err := os.WriteFile(dockerfileDst, dockerfileContent, 0644); err != nil {
+
+	if err := os.WriteFile(dockerfileDst, []byte(dockerfileContent), 0644); err != nil {
 		fmt.Printf("Error writing Dockerfile: %v\n", err)
 		return
 	}
